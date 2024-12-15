db534891ede3fec331b8a7efb813b8ea20b6529a
VanderG-dev <VanderG.dev@gmail.com>
2024-12-15 18:11:38 +0600

    test

--- Backend/server.js
+++ Backend/server.js
@@ -58,10 +58,10 @@ app.get("/test", (req, res) => {
   `);
 });
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "../Frontent/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/index.html"));
+  res.sendFile(path.join(__dirname, "../Frontent/dist/index.html"));
 });
 
 // Start the server
