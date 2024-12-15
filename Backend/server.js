08dd6a24e54329f8d05d31c2623fd7435f008bf9
VanderG-dev <VanderG.dev@gmail.com>
2024-12-15 16:36:57 +0600

    test

--- Backend/server.js
+++ Backend/server.js
@@ -59,11 +59,11 @@ app.get("/test", (req, res) => {
 });
 
 // Serve static files from the frontend build
-app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+app.use(express.static(path.join(__dirname, "../Frontend")));
 
 // Serve the frontend on all other routes
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../Frontend/index.html"));
 });
 
 // Start the server
